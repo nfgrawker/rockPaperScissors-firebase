ef1ea8f648a7fe5136f103cf4f4dfc10eb76b2eb
nfgrawker <nfgrawker@gmail.com>
2018-10-02 20:09:13 -0500

    Initial structure created. HTML and basic css done. Linked all scripts and started firebase api.

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyCX-4WMc7GO4atnzhis_tQwlCJ9D7L3H8g",
+    authDomain: "inclassfirebase-be734.firebaseapp.com",
+    databaseURL: "https://inclassfirebase-be734.firebaseio.com",
+    projectId: "inclassfirebase-be734",
+    storageBucket: "inclassfirebase-be734.appspot.com",
+    messagingSenderId: "308594412083"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database()
+  
