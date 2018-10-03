988a6de9874541303f7fa6288c90b81f8b42dec9
nfgrawker <nfgrawker@gmail.com>
2018-10-03 09:07:02 -0500

    Working on chat functionality with firebase.push and limittolast

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+
 var config = {
     apiKey: "AIzaSyCX-4WMc7GO4atnzhis_tQwlCJ9D7L3H8g",
     authDomain: "inclassfirebase-be734.firebaseapp.com",
@@ -6,6 +7,37 @@ var config = {
     storageBucket: "inclassfirebase-be734.appspot.com",
     messagingSenderId: "308594412083"
   };
-  firebase.initializeApp(config);
-  var database = firebase.database()
-  
+firebase.initializeApp(config);
+var database = firebase.database()
+var currentchat = ""
+
+$('.chattext').keypress(function(event){
+      var keycode = (event.keyCode ? event.keyCode : event.which);
+      if(keycode == '13'){
+      currentchat = $(this).val()
+      $(this).val("")
+      $(".chatbox").empty()
+      database.ref().push({
+        chat: currentchat,
+        dateAdded : firebase.database.ServerValue.TIMESTAMP
+      })
+      }
+  });
+database.ref().on("child_added", function(childSnapshot) {
+
+   // Log everything that's coming out of snapshot
+   console.log(childSnapshot.val().chat);
+   console.log(childSnapshot.val().dateAdded);
+
+})
+database.ref().orderByChild("dateAdded").limitToLast(10).on("child_added", function(snapshot) {
+  $(".chatbox").prepend("<p>"+snapshot.val().chat+"</p>");
+  $(".temp").remove()
+
+});
+
+  $(".clear").on("click",function() {
+    database.ref().remove()
+    $(".chatbox").empty()
+    $(".chatbox").append("<h2 class ='temp'>Chat Appears Here!</h2>")
+  })
